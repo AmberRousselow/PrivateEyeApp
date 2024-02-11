39a485688ccbfbabc4225fbbab821339815d71a9
AmberRousselow <73440034+AmberRousselow@users.noreply.github.com>
2024-02-11 16:09:43 -0600

    deleted:    .graphqlconfig.yml 	deleted:    amplify/backend/api/privateeyeapp/cli-inputs.json 	deleted:    amplify/backend/api/privateeyeapp/parameters.json 	deleted:    amplify/backend/api/privateeyeapp/resolvers/README.md 	deleted:    amplify/backend/api/privateeyeapp/schema.graphql 	deleted:    amplify/backend/api/privateeyeapp/stacks/CustomResources.json 	deleted:    amplify/backend/api/privateeyeapp/transform.conf.json 	modified:   amplify/backend/backend-config.json 	modified:   amplify/backend/types/amplify-dependent-resources-ref.d.ts 	modified:   amplify/team-provider-info.json

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,11 +1,4 @@
 export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "privateeyeapp": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  },
   "auth": {
     "privateeyeappdc8c26a9": {
       "AppClientID": "string",
