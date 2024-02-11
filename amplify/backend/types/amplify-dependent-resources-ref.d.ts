2abeb033be6de2571c1c5042f020645ee371a873
AmberRousselow <73440034+AmberRousselow@users.noreply.github.com>
2024-02-11 17:34:50 -0600

    Added graphql and storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "FileStorage": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
