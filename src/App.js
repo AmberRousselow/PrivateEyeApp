c02fa30e72591e8099aa521b1beaeb3d3b3ad9c1
AmberRousselow <73440034+AmberRousselow@users.noreply.github.com>
2024-03-29 22:07:57 -0500

    modified:   src/App.css 	modified:   src/App.js 	modified:   src/ui-components/CaseDetailHeader.d.ts 	modified:   src/ui-components/CaseDetailHeader.jsx

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import textLogoHead from './Images/PElogoText.png';
 import ladyAvatar from './Images/ai-generated-8534133_1280.jpg';
 import fingerprintImg2 from './Images/fingerprint-255899_1280.jpg';
 import suspectImg1 from './Images/hacker-3342696_1280.jpg';
+import detective from './Images/detective-4787272_1280.png';
+import manImg from './Images/man-8537038_1280.png';
 import {
   CaseCardCollection,
   CaseDetailHeader,
@@ -32,6 +34,7 @@ import {
   View,
   withAuthenticator,
 } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
 
 const App = ({ signOut }) => {
 
@@ -95,6 +98,7 @@ const App = ({ signOut }) => {
     }
   };
 
+
   /*****HOOKS****/
   //SIDE BAR
   const [showSidebar, setShowSidebar] = useState(false);
@@ -121,20 +125,31 @@ const App = ({ signOut }) => {
   //setList of Suspects linked to Case
   const [getListSuspect, setListSuspect] = useState();
 
+  const [dayFact, setDayFact] = useState(calcDays());
+  const [susFact, setSusFact] = useState("");
+  const [evFact, setEvFact] = useState("");
+  const [statFact, setStatFact] = useState("");
+
+
+  function calcDays(createdDate) {
+    const today = new Date();
+    var returnDate = createdDate - today
+    return returnDate;
+  }
 
   useEffect(() => { }, []);
 
   /**HANDLE CLICKS**/
   // view SIDE BAR
   const toggleSidebar = () => {
-    setShowSidebar(!showSidebar);
+    setShowSidebar(true);
   };
 
   // Function to toggle default HOME view
   const toggleHomeView = () => {
     console.log("Toggle Home"); // Add logging to check if the function is called
     setShowHome(true); // #1 Home True
-    setShowSidebar(!showSidebar);
+    setShowSidebar(false);
     setShowCreateCase(false); // #2 Set showCreateCase to true
     setShowCreateNote(false); // #3 Set showCreateNote to false
     setShowDetailedCaseView(false); // #4 Set showDetailedCaseView to false
@@ -147,6 +162,7 @@ const App = ({ signOut }) => {
   const toggleCreateCase = () => {
     console.log("Toggle Create Case Clicked");
     setShowHome(false);
+    setShowSidebar(false);
     setShowCreateCase(true); // #2 Create Case
     setShowCreateNote(false);
     setShowDetailedCaseView(false);
@@ -159,6 +175,7 @@ const App = ({ signOut }) => {
   const toggleCreateNote = () => {
     console.log("Toggle Create Note Clicked");
     setShowHome(false);
+    setShowSidebar(false);
     setShowCreateCase(false);
     setShowCreateNote(true); // #3 Create Note
     setShowDetailedCaseView(false);
@@ -171,6 +188,7 @@ const App = ({ signOut }) => {
   const handleCaseViewButtonClick = () => {
     console.log("View button clicked on Case");
     setShowHome(false);
+    setShowSidebar(false);
     setShowCreateCase(false);
     setShowCreateNote(false);
     setShowDetailedCaseView(true); // #4 Detailed Case View
@@ -182,6 +200,7 @@ const App = ({ signOut }) => {
   const handleCaseHeaderClick = () => {
     console.log("Cases on Nav Bar Clicked");
     setShowHome(false);
+    setShowSidebar(false);
     setShowCreateCase(false);
     setShowCreateNote(false);
     setShowDetailedCaseView(false);
@@ -194,6 +213,7 @@ const App = ({ signOut }) => {
   const handleSuspectsHeaderClick = () => {
     console.log("Suspects on Nav Bar Clicked");
     setShowHome(false);
+    setShowSidebar(false);
     setShowCreateCase(false);
     setShowCreateNote(false);
     setShowDetailedCaseView(false);
@@ -206,6 +226,7 @@ const App = ({ signOut }) => {
   const handleSuspectsDetailClick = () => {
     console.log("View Suspects button Clicked");
     setShowHome(false);
+    setShowSidebar(false);
     setShowCreateCase(false);
     setShowCreateNote(false);
     setShowDetailedCaseView(false);
@@ -219,7 +240,7 @@ const App = ({ signOut }) => {
   const sideBarView = showSidebar ? (
     <div>
       <SideBar
-        class="sidebar"
+        className="sidebar"
         overrides={{ //SIDEBAR user clicks
           "label39493362": {
             className: "custom-btn",
@@ -280,12 +301,10 @@ const App = ({ signOut }) => {
   /*CREATE CASE FORM*/
   const createCaseView = showCreateCase ? (
     <div>
-      <View style={{ display: 'flex', flexDirection: 'row' }}>
+      <Heading level={2} className="special-elite-regular" fontSize={"40px"}>Create Case</Heading>
+      <View>
         {/* Create Case Section */}
-        <View style={{ flex: 1, marginLeft: 10 }}>
-          <Heading level={2} class="special-elite-regular" fontSize={"40px"}>Create Case</Heading>
-          <NewCaseCreateForm class="create-form" />
-        </View>
+        <NewCaseCreateForm justifyContent="center" className="create-form" />
       </View>
     </div>
   ) : null; // Render null if showDetailedCaseView is false
@@ -293,16 +312,13 @@ const App = ({ signOut }) => {
   /*CREATE NOTE FORM*/
   const createNoteView = showCreateNote ? (
     <div>
-      <View style={{ display: 'flex', flexDirection: 'row' }}>
-        {/* Create Note Section */}
-        <View style={{ flex: 1, marginRight: 10 }}>
-          <Heading level={2} class="special-elite-regular" fontSize={"40px"}>Add Case Note</Heading>
-          <CaseNoteCreateForm />
-        </View>
+      {/* Create Note Section */}
+      <View>
+        <Heading level={2} class="special-elite-regular" fontSize={"40px"}>Add Case Note</Heading>
+        <CaseNoteCreateForm className="create-form" />
       </View>
     </div>
   ) : null; // Render null if showDetailedCaseView is false
-
 
   /*ALL CASE VIEW**/
   const allCaseView = showAllCaseView ? (
@@ -331,22 +347,70 @@ const App = ({ signOut }) => {
   /*CASE DETAIL VIEW*/
   const detailedCaseView = showDetailedCaseView ? (
     <div>
-      <View style={{ flex: 1, marginLeft: 10, marginRight: 50 }}>
-        <CaseDetailHeader appCase={getAppCase} width={"80%"} alignItems={"center"} marginTop={"40p"} marginBottom={"2px"}></CaseDetailHeader>
-      </View>
-      <View style={{ display: 'flex', flexDirection: 'row' }}>
-        {/* Suspects Section */}
-        <View style={{ flex: 1, marginLeft: 50, marginRight: 10 }}>
-          <Heading level={2} class="special-elite-regular" fontSize={"40px"}>Suspects</Heading>
-          <SuspectCollection ></SuspectCollection>
-        </View>
+      <View style={{ flex: 1, marginLeft: '300px', marginRight: '10px', width: '80%' }}>
+        <CaseDetailHeader appCase={getAppCase} justifyContent={"center"} alignItems={"center"} marginTop={"40px"} marginBottom={"2px"}
+          overrides={{
+            "CaseOverviewHeader": {
+              children: "Case Overview"
+            },
+            "DaysFact": {
+              children: dayFact
+            },
+            "SusFact": {
+              children: susFact
+            },
+            "EvFact": {
+              children: evFact
+            },
+            "StatusFact": {
+              children: statFact
+            }
+          }
+          }
+        />
+        <View style={{ display: 'flex', flexDirection: 'row' }}>
+          {/* Suspects Section */}
+          <View style={{ flex: 1, marginLeft: 50, marginRight: 10 }}>
+            <Heading level={2} class="special-elite-regular" fontSize={"40px"}>Suspects</Heading>
+            <SuspectCollection overrideItems={({ item, index }) => {
+              console.log("Suspect detail");
+              return {
+                overrides: {
+                  "Button": {
+                    onClick: () => {
+                      setSuspect(item);
+                      console.log("suspect ");
+                      handleSuspectsDetailClick();
+                    },
+                  },
+                  "image": {
+                    src: suspectImg1,
+                  }
+                },
+              };
+            }} />
+          </View>
 
-        {/* Create Case Section */}
-        <View style={{ flex: 1, marginLeft: 10, marginRight: 50 }}>
-          <Heading level={2} class="special-elite-regular" fontSize={"40px"}>Evidence</Heading>
-          <EvidenceCollection />
+          {/* Evidence Section */}
+          <View style={{ flex: 1, marginLeft: 10, marginRight: 50 }}>
+            <Heading level={2} class="special-elite-regular" fontSize={"40px"}>Evidence</Heading>
+            <EvidenceCollection overrideItems={({ item, index }) => {
+              console.log("Evidence detail");
+              return {
+                overrides: {
+                  "Button": {
+                    onClick: () => {
+                      console.log("Evidence ");
+                    },
+                  },
+                  "image": {
+                    src: manImg,
+                  }
+                },
+              };
+            }} />
+          </View>
         </View>
-
       </View>
     </div>
   ) : null; // Render null
@@ -383,7 +447,7 @@ const App = ({ signOut }) => {
     <div>
       <Flex direction="Column" justifyContent="Center" alignItems="Center" >
         <Heading level={2} marginTop={"40px"} marginBottom={"40px"} class="special-elite-regular" fontSize={"40px"}>Suspect Details</Heading>
-        <SuspectDetail suspect={getSuspect}></SuspectDetail>
+        <SuspectDetail suspect={getSuspect} ></SuspectDetail>
       </Flex>
     </div>
   ) : null;
@@ -391,12 +455,17 @@ const App = ({ signOut }) => {
   /********RENDER ON PAGE*********/
   return (
     <View className="App">
-
       {/*** HEADER *** ALWAYS DISPLAY ***/}
       <NavBarHeader overrides={navbarOverrides} width={"100vw"} marginTop={"40p"} marginBottom={"2px"}></NavBarHeader>
-      <main>
-        <FontAwesomeIcon icon={showSidebar ? faTimes : faArrowRight} onClick={toggleSidebar} className="sidebar-icon" />
-        {sideBarView}
+      {/*** MAIN *** UNDER HEADER AND ABOVE FOOTER ***/}
+      <main className={showSidebar ? "main-content sidebar-open" : "main-content"}>
+        <div className={"sidebar-container"}>
+          <div className={showSidebar ? "sidebar open" : "sidebar closed"}>
+            {/* Sidebar */}
+            {sideBarView}
+          </div>
+          <FontAwesomeIcon icon={showSidebar ? faTimes : faArrowRight} onClick={toggleSidebar} className={showSidebar ? "sidebar-icon open" : "sidebar-icon closed"} />
+        </div>
         {homeView}
         {createCaseView}
         {createNoteView}
@@ -405,6 +474,7 @@ const App = ({ signOut }) => {
         {allSuspectView}
         {suspectDetailView}
       </main>
+      {/*** FOOTER *** ALWAYS DISPLAY***/}
       <MarketingFooterBrand overrides={footerOverrides} className="footer" width={"100vw"} marginTop={"40p"} >
       </MarketingFooterBrand>
     </View>
--- src/ui-components/CaseDetailHeader.jsx
+++ src/ui-components/CaseDetailHeader.jsx
@@ -19,17 +19,20 @@ export default function CaseDetailHeader(props) {
       overrides: {
         CaseOverviewHeader: {},
         DaysFact: {},
-        SusFact: {},
-        EvFact: {},
-        StatusFact: {},
-        StatFacts: {},
         DaysSinceOpened: {},
+        "Frame 4374": {},
+        SusFact: {},
         Suspects: {},
+        "Frame 4375": {},
+        EvFact: {},
         EvidenceCollected: {},
+        "Frame 4376": {},
+        StatusFact: {},
         Status: {},
-        StatHeaders: {},
-        "Frame 438": {},
+        "Frame 4377": {},
+        StatFacts: {},
         Description: {},
+        "Frame 438": {},
         Button: {},
         CaseDetailHeader: {},
       },
@@ -44,8 +47,8 @@ export default function CaseDetailHeader(props) {
     <Flex
       gap="35px"
       direction="column"
-      width="1440px"
-      height="unset"
+      width="1438px"
+      height="525px"
       justifyContent="center"
       alignItems="center"
       overflow="hidden"
@@ -53,6 +56,7 @@ export default function CaseDetailHeader(props) {
       boxShadow="0px 4px 4px rgba(0, 0, 0, 0.25)"
       padding="0px 0px 0px 0px"
       display="flex"
+      dateStat={appCase?.case_created_date}
       {...getOverrideProps(overrides, "CaseDetailHeader")}
       {...rest}
     >
@@ -60,254 +64,298 @@ export default function CaseDetailHeader(props) {
         fontFamily="Inter"
         fontSize="85px"
         fontWeight="800"
-        color="rgba(37,30,25,1)"
+        color="rgba(78,67,59,1)"
         lineHeight="106.25px"
         textAlign="left"
         display="block"
         direction="column"
         justifyContent="unset"
-        width="1440px"
+        width="unset"
         height="106px"
         gap="unset"
         alignItems="unset"
         shrink="0"
+        alignSelf="stretch"
         position="relative"
         padding="0px 0px 0px 0px"
         whiteSpace="pre-wrap"
-        children="Case Overview"
         {...getOverrideProps(overrides, "CaseOverviewHeader")}
       ></Text>
       <Flex
-        gap="0"
+        gap="2px"
         direction="column"
         width="unset"
         height="unset"
         justifyContent="center"
         alignItems="center"
+        overflow="hidden"
         shrink="0"
         position="relative"
-        padding="0px 0px 0px 0px"
+        padding="5px 0px 5px 0px"
         display="flex"
         {...getOverrideProps(overrides, "Frame 438")}
       >
         <Flex
-          gap="0"
+          gap="17px"
           direction="row"
           width="unset"
           height="unset"
           justifyContent="center"
           alignItems="center"
+          overflow="hidden"
           shrink="0"
           position="relative"
           padding="0px 0px 0px 0px"
           display="flex"
           {...getOverrideProps(overrides, "StatFacts")}
         >
-          <Text
-            fontFamily="Inter"
-            fontSize="85px"
-            fontWeight="800"
-            color="rgba(99,29,20,1)"
-            lineHeight="106.25px"
-            textAlign="center"
-            display="block"
+          <Flex
+            gap="0"
             direction="column"
-            justifyContent="unset"
-            width="307px"
-            height="101px"
-            gap="unset"
-            alignItems="unset"
+            width="unset"
+            height="unset"
+            justifyContent="center"
+            alignItems="center"
             shrink="0"
             position="relative"
             padding="0px 0px 0px 0px"
-            whiteSpace="pre-wrap"
-            children="777"
-            {...getOverrideProps(overrides, "DaysFact")}
-          ></Text>
-          <Text
-            fontFamily="Inter"
-            fontSize="85px"
-            fontWeight="800"
-            color="rgba(99,29,20,1)"
-            lineHeight="106.25px"
-            textAlign="center"
-            display="block"
+            display="flex"
+            {...getOverrideProps(overrides, "Frame 4374")}
+          >
+            <Text
+              fontFamily="Inter"
+              fontSize="85px"
+              fontWeight="800"
+              color="rgba(99,29,20,1)"
+              lineHeight="106.25px"
+              textAlign="center"
+              display="block"
+              direction="column"
+              justifyContent="unset"
+              width="307px"
+              height="101px"
+              gap="unset"
+              alignItems="unset"
+              shrink="0"
+              position="relative"
+              padding="0px 0px 0px 0px"
+              whiteSpace="pre-wrap"
+              children="777"
+              {...getOverrideProps(overrides, "DaysFact")}
+            ></Text>
+            <Text
+              fontFamily="Inter"
+              fontSize="20px"
+              fontWeight="700"
+              color="rgba(0,0,0,1)"
+              lineHeight="25px"
+              textAlign="center"
+              display="block"
+              direction="column"
+              justifyContent="unset"
+              width="307px"
+              height="101px"
+              gap="unset"
+              alignItems="unset"
+              shrink="0"
+              position="relative"
+              padding="0px 0px 0px 0px"
+              whiteSpace="pre-wrap"
+              children="Days Since Opened"
+              {...getOverrideProps(overrides, "DaysSinceOpened")}
+            ></Text>
+          </Flex>
+          <Flex
+            gap="0"
             direction="column"
-            justifyContent="unset"
-            width="307px"
-            height="101px"
-            gap="unset"
-            alignItems="unset"
+            width="unset"
+            height="unset"
+            justifyContent="center"
+            alignItems="center"
             shrink="0"
             position="relative"
             padding="0px 0px 0px 0px"
-            whiteSpace="pre-wrap"
-            children="3"
-            {...getOverrideProps(overrides, "SusFact")}
-          ></Text>
-          <Text
-            fontFamily="Inter"
-            fontSize="85px"
-            fontWeight="800"
-            color="rgba(99,29,20,1)"
-            lineHeight="106.25px"
-            textAlign="center"
-            display="block"
+            display="flex"
+            {...getOverrideProps(overrides, "Frame 4375")}
+          >
+            <Text
+              fontFamily="Inter"
+              fontSize="85px"
+              fontWeight="800"
+              color="rgba(99,29,20,1)"
+              lineHeight="106.25px"
+              textAlign="center"
+              display="block"
+              direction="column"
+              justifyContent="unset"
+              width="307px"
+              height="101px"
+              gap="unset"
+              alignItems="unset"
+              shrink="0"
+              position="relative"
+              padding="0px 0px 0px 0px"
+              whiteSpace="pre-wrap"
+              children="3"
+              {...getOverrideProps(overrides, "SusFact")}
+            ></Text>
+            <Text
+              fontFamily="Inter"
+              fontSize="20px"
+              fontWeight="700"
+              color="rgba(0,0,0,1)"
+              lineHeight="25px"
+              textAlign="center"
+              display="block"
+              direction="column"
+              justifyContent="unset"
+              width="307px"
+              height="101px"
+              gap="unset"
+              alignItems="unset"
+              shrink="0"
+              position="relative"
+              padding="0px 0px 0px 0px"
+              whiteSpace="pre-wrap"
+              children="Suspects"
+              {...getOverrideProps(overrides, "Suspects")}
+            ></Text>
+          </Flex>
+          <Flex
+            gap="0"
             direction="column"
-            justifyContent="unset"
-            width="307px"
-            height="101px"
-            gap="unset"
-            alignItems="unset"
+            width="unset"
+            height="unset"
+            justifyContent="center"
+            alignItems="center"
             shrink="0"
             position="relative"
             padding="0px 0px 0px 0px"
-            whiteSpace="pre-wrap"
-            children="150"
-            {...getOverrideProps(overrides, "EvFact")}
-          ></Text>
-          <Text
-            fontFamily="Inter"
-            fontSize="85px"
-            fontWeight="800"
-            color="rgba(99,29,20,1)"
-            lineHeight="106.25px"
-            textAlign="center"
-            display="block"
+            display="flex"
+            {...getOverrideProps(overrides, "Frame 4376")}
+          >
+            <Text
+              fontFamily="Inter"
+              fontSize="85px"
+              fontWeight="800"
+              color="rgba(99,29,20,1)"
+              lineHeight="106.25px"
+              textAlign="center"
+              display="block"
+              direction="column"
+              justifyContent="unset"
+              width="307px"
+              height="101px"
+              gap="unset"
+              alignItems="unset"
+              shrink="0"
+              position="relative"
+              padding="0px 0px 0px 0px"
+              whiteSpace="pre-wrap"
+              children="150"
+              {...getOverrideProps(overrides, "EvFact")}
+            ></Text>
+            <Text
+              fontFamily="Inter"
+              fontSize="20px"
+              fontWeight="700"
+              color="rgba(0,0,0,1)"
+              lineHeight="25px"
+              textAlign="center"
+              display="block"
+              direction="column"
+              justifyContent="unset"
+              width="307px"
+              height="101px"
+              gap="unset"
+              alignItems="unset"
+              shrink="0"
+              position="relative"
+              padding="0px 0px 0px 0px"
+              whiteSpace="pre-wrap"
+              children="Evidence Collected"
+              {...getOverrideProps(overrides, "EvidenceCollected")}
+            ></Text>
+          </Flex>
+          <Flex
+            gap="0"
             direction="column"
-            justifyContent="unset"
-            width="307px"
-            height="101px"
-            gap="unset"
-            alignItems="unset"
+            width="unset"
+            height="unset"
+            justifyContent="center"
+            alignItems="center"
             shrink="0"
             position="relative"
             padding="0px 0px 0px 0px"
-            whiteSpace="pre-wrap"
-            children="OPEN"
-            {...getOverrideProps(overrides, "StatusFact")}
-          ></Text>
+            display="flex"
+            {...getOverrideProps(overrides, "Frame 4377")}
+          >
+            <Text
+              fontFamily="Inter"
+              fontSize="85px"
+              fontWeight="800"
+              color="rgba(99,29,20,1)"
+              lineHeight="106.25px"
+              textAlign="center"
+              display="block"
+              direction="column"
+              justifyContent="unset"
+              width="307px"
+              height="101px"
+              gap="unset"
+              alignItems="unset"
+              shrink="0"
+              position="relative"
+              padding="0px 0px 0px 0px"
+              whiteSpace="pre-wrap"
+              children="OPEN"
+              {...getOverrideProps(overrides, "StatusFact")}
+            ></Text>
+            <Text
+              fontFamily="Inter"
+              fontSize="20px"
+              fontWeight="700"
+              color="rgba(0,0,0,1)"
+              lineHeight="25px"
+              textAlign="center"
+              display="block"
+              direction="column"
+              justifyContent="unset"
+              width="307px"
+              height="101px"
+              gap="unset"
+              alignItems="unset"
+              shrink="0"
+              position="relative"
+              padding="0px 0px 0px 0px"
+              whiteSpace="pre-wrap"
+              children="Status"
+              {...getOverrideProps(overrides, "Status")}
+            ></Text>
+          </Flex>
         </Flex>
-        <Flex
-          gap="0"
-          direction="row"
-          width="unset"
-          height="unset"
-          justifyContent="center"
-          alignItems="center"
+        <Text
+          fontFamily="Special Elite"
+          fontSize="16px"
+          fontWeight="400"
+          color="rgba(0,0,0,1)"
+          lineHeight="24px"
+          textAlign="center"
+          display="block"
+          direction="column"
+          justifyContent="unset"
+          width="1336px"
+          height="72px"
+          gap="unset"
+          alignItems="unset"
           shrink="0"
           position="relative"
           padding="0px 0px 0px 0px"
-          display="flex"
-          {...getOverrideProps(overrides, "StatHeaders")}
-        >
-          <Text
-            fontFamily="Inter"
-            fontSize="20px"
-            fontWeight="700"
-            color="rgba(0,0,0,1)"
-            lineHeight="25px"
-            textAlign="center"
-            display="block"
-            direction="column"
-            justifyContent="unset"
-            width="307px"
-            height="101px"
-            gap="unset"
-            alignItems="unset"
-            shrink="0"
-            position="relative"
-            padding="0px 0px 0px 0px"
-            whiteSpace="pre-wrap"
-            children="Days Since Opened"
-            {...getOverrideProps(overrides, "DaysSinceOpened")}
-          ></Text>
-          <Text
-            fontFamily="Inter"
-            fontSize="20px"
-            fontWeight="700"
-            color="rgba(0,0,0,1)"
-            lineHeight="25px"
-            textAlign="center"
-            display="block"
-            direction="column"
-            justifyContent="unset"
-            width="307px"
-            height="101px"
-            gap="unset"
-            alignItems="unset"
-            shrink="0"
-            position="relative"
-            padding="0px 0px 0px 0px"
-            whiteSpace="pre-wrap"
-            children="Suspects"
-            {...getOverrideProps(overrides, "Suspects")}
-          ></Text>
-          <Text
-            fontFamily="Inter"
-            fontSize="20px"
-            fontWeight="700"
-            color="rgba(0,0,0,1)"
-            lineHeight="25px"
-            textAlign="center"
-            display="block"
-            direction="column"
-            justifyContent="unset"
-            width="307px"
-            height="101px"
-            gap="unset"
-            alignItems="unset"
-            shrink="0"
-            position="relative"
-            padding="0px 0px 0px 0px"
-            whiteSpace="pre-wrap"
-            children="Evidence Collected"
-            {...getOverrideProps(overrides, "EvidenceCollected")}
-          ></Text>
-          <Text
-            fontFamily="Inter"
-            fontSize="20px"
-            fontWeight="700"
-            color="rgba(0,0,0,1)"
-            lineHeight="25px"
-            textAlign="center"
-            display="block"
-            direction="column"
-            justifyContent="unset"
-            width="307px"
-            height="101px"
-            gap="unset"
-            alignItems="unset"
-            shrink="0"
-            position="relative"
-            padding="0px 0px 0px 0px"
-            whiteSpace="pre-wrap"
-            children="Status"
-            {...getOverrideProps(overrides, "Status")}
-          ></Text>
-        </Flex>
+          whiteSpace="pre-wrap"
+          children={appCase?.case_description}
+          {...getOverrideProps(overrides, "Description")}
+        ></Text>
       </Flex>
-      <Text
-        fontFamily="Inter"
-        fontSize="16px"
-        fontWeight="400"
-        color="rgba(0,0,0,1)"
-        lineHeight="24px"
-        textAlign="center"
-        display="block"
-        direction="column"
-        justifyContent="unset"
-        width="1228px"
-        height="107px"
-        gap="unset"
-        alignItems="unset"
-        shrink="0"
-        position="relative"
-        padding="0px 0px 0px 0px"
-        whiteSpace="pre-wrap"
-        children={appCase?.case_description}
-        {...getOverrideProps(overrides, "Description")}
-      ></Text>
       <Button
         width="unset"
         height="unset"
